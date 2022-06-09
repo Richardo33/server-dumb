1375cda92e804ddb509ea38e0f50c635b58afc62
hackerrank <me@hackerrank.com>
2022-06-09 11:35:36 +0700

    update

---
+++ models/productcategory.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class productcategory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  product -
+    category.init(
+      {
+        idProduct: DataTypes.INTEGER,
+        idCategory: DataTypes.INTEGER,
+      },
+      {
+        sequelize,
+        modelName: "product-category",
+      }
+    );
+  return product - category;
+};
